3c8c200e48cd9efc328f6cf5093d00f90d97cbc9
Ashish Kumar <96125918+ashish7475@users.noreply.github.com>
2023-07-21 03:53:12 +0530

    group chats socket  working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { Server } from "socket.io";
 import http from "http";
 import cors from "cors";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 app.use(cors());
@@ -29,10 +30,32 @@ io.on("connection", (socket) => {
     io.emit("get-users", { activeUsers, connectedUser: { ...user } });
   });
 
+  //! Send message to all the online users about group
+  socket.on("join_group_multiple", (data) => {
+    const users = data.users;
+    const roomId = uuidv4();
+    const socketIds = users.map(
+      (user) => activeUsers.find((usr) => usr.userId === user.userId).socketId
+    );
+    socketIds.forEach((socketId) => {});
+  });
+  //! for every user - on groupChat click join the room using groupId
+  socket.on("join_group", (data) => {
+    const group = data.activeGroup;
+    socket.join(group.groupId);
+    console.log("Group Joined", data);
+  });
+  //? socket code for group chat
+  socket.on("send_group_message", (data) => {
+    console.log("Group Message Send", data);
+    socket.to(data.groupId).emit("receive_group_message", data);
+    //! write socket code for emitting receive group chat to frontend
+  });
+
   socket.on("send_message", (data) => {
     const { userId } = data;
     const user = activeUsers.find((user) => user.userId === userId);
-    
+
     if (user) {
       io.to(user.socketId).emit("recieve_message", data);
     }
