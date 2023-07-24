50283882be05b174ef1a5939f90f162da7253e88
Shubham Sangwan <79131438+shubhamSangwan01@users.noreply.github.com>
2023-07-24 08:01:11 +0530

    client link upadted

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://smartchatappserver-production.up.railway.app/",
+    origin: "https://main--bright-macaron-9e9881.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
